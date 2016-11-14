e9f400cae9fa5fb4da54700795ab2dc4760bf306
12zoya <z_rudashevskaya@mail.ru>
2016-11-14 19:05:10 +0300

    доделано

---
+++ src/com/company/Main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	Deck deck = new Deck();
+        System.out.println(deck);
+        }
+    }
---
+++ src/com/company/Value.java
@@ -0,0 +1,11 @@
+package com.company;
+
+/**
+ * Created by student2 on 11.11.16.
+ */
+public enum Value {
+    TWO, THREE, FOOR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN,
+    JACK, QUEEN, KING,
+    ACE
+
+}
