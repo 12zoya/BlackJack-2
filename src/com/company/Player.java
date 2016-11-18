f53bb897e7e02e188bef5a906dc83f3c513a29e3
12zoya <z_rudashevskaya@mail.ru>
2016-11-18 19:11:25 +0300

    18.11.2016 - КАК СЛОЖНООО Начала описывать основной процесс игры. Описала связи между интеллектами. Продолжу работу над LimitIntellect, чтобы игроки уже могли играть.

--- src/com/company/Player.java
+++ src/com/company/Player.java
@@ -4,4 +4,18 @@ package com.company;
  * Created by student2 on 14.11.16.
  */
 public abstract class Player  {
+    Hand hand = new Hand();
+    private Intellect intellect;
+
+    public void take(Card current) {
+        hand.add(current);
+    }
+
+    public Player(Intellect intellect) {
+        this.intellect = intellect;
+    }
+
+    public Command decision() {
+        return intellect.decide();
+    }
 }
